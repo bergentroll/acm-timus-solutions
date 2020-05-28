3ac5f8e41f055341be19e460e2ac73707d323900
Anton Karmanov <bergertroll@insiberia.net>
2020-05-28 22:29:55 +0500

    Solve 2069

--- 2069.cpp
+++ 2069.cpp
@@ -5,38 +5,6 @@
 
 using namespace std;
 
-class Solver {
-  public:
-  Solver(vector<unsigned> const &vert, vector<unsigned> const &hor):
-  vert(vert),
-  hor(hor),
-  target_x(vert.size() - 1),
-  target_y(hor.size() - 1) { }
-
-  void traverse(unsigned x, unsigned y, unsigned min_val) {
-    if (x == target_x && y == target_y) {
-      result = max(result, min_val);
-      return;
-    }
-
-    if (min_val <= result) return;
-
-    if (x < target_x) {
-      traverse(x + 1, y, min(min_val, hor[y]));
-    }
-    if (y < target_y) {
-      traverse(x, y + 1, min(min_val, vert[x]));
-    }
-  }
-
-  unsigned getResult() { return result; }
-
-  private:
-  vector<unsigned> const &vert, &hor;
-  unsigned target_x, target_y;
-  unsigned result { numeric_limits<unsigned>::min() };
-};
-
 int main() {
   unsigned n, m;
   cin >> n >> m;
@@ -53,11 +21,12 @@ int main() {
     hor.push_back(buf);
   }
 
-  Solver s { vert, hor };
+  unsigned var1 { max(min(vert.front(), hor.back()), min(vert.back(), hor.front())) };
+  unsigned var2 { max(
+    min(min(vert.front(), vert.back()), *max_element(next(hor.begin()), prev(hor.end()))),
+    min(min(hor.front(), hor.back()), *max_element(next(vert.begin()), prev(vert.end())))) };
 
-  s.traverse(0, 0, numeric_limits<unsigned>::max());
-
-  cout << s.getResult();
+  cout << max(var1, var2);
 
   return EXIT_SUCCESS;
 }

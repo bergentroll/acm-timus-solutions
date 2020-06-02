588e4ceecc93b5ed457d0b8dd2b9e27909e26ff4
Anton Karmanov <bergertroll@insiberia.net>
2020-06-02 20:35:47 +0500

    Solve 1769

--- 1769.cpp
+++ 1769.cpp
@@ -1,19 +1,34 @@
 #include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
+size_t MAX_LENGTH { 6 };
+
 int main() {
-  string s;
-
-  cin >> s;
-
+  string stone;
   unsigned result { 1 };
-  while(true) {
-    auto it { s.find(to_string(result)) };
-    if (it == s.npos) break;
+  unordered_set<string> grinder;
+
+  cin >> stone;
+
+  size_t stone_length { stone.length() };
+  size_t max_length { min(MAX_LENGTH, stone_length) };
+
+  for(size_t length { 1 }; length < max_length + 1; ++length) {
+    for(size_t i { 0 }; i < stone_length - length + 1; ++i) {
+      auto val { stone.substr(i, length) };
+      grinder.insert(val);
+    }
+  }
+
+  while (true) {
+    if (grinder.count(to_string(result)) == 0) {
+      cout << result;
+      break;
+    }
     ++result;
   }
 
-  cout << result;
   return EXIT_SUCCESS;
 }

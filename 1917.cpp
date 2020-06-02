126284ea47babad0732e0c53dee6efad8c297d71
Anton Karmanov <bergertroll@insiberia.net>
2020-06-02 22:23:14 +0500

    Solve 1917

---
+++ 1917.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int main() {
+  unsigned n, p;
+  map<unsigned, unsigned> a;
+
+  cin >> n >> p;
+
+  for (unsigned i { }; i < n; ++i) {
+    unsigned buf;
+    cin >> buf;
+    ++a[buf];
+  }
+
+  unsigned
+    acc { },
+    total_amount { },
+    cast_num { };
+
+  for (auto it { a.cbegin() }; it != a.cend(); ++it) {
+    unsigned power { (acc + it->second) * it->first};
+
+    if (power > p) {
+      if (acc == 0) break;
+
+      total_amount += acc;
+      ++cast_num;
+      acc = 0;
+      --it;
+    }
+    else {
+      acc += it->second;
+    }
+  }
+
+  total_amount += acc;
+  cast_num += (acc > 0);
+
+  cout << total_amount << ' ' << cast_num;
+
+  return EXIT_SUCCESS;
+}

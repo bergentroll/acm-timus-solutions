6b169aa03dbb77fb7f77d7d64bfb6dc02ce986f3
Anton Karmanov <bergertroll@insiberia.net>
2020-06-03 20:55:42 +0500

    Add traversal for 1033

---
+++ 1033.cpp
@@ -0,0 +1,79 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using Map = vector<vector<bool>>;
+
+void print(Map const &map) {
+  for (auto i: map) {
+    for (auto j: i) {
+      cerr << (j? '+': '-');
+    }
+    cerr << endl;
+  }
+  cerr << endl;
+}
+
+void traverse(Map const &map, Map &visited, int x, int y) {
+  visited[x][y] = true;
+
+  auto max_index { map.size() };
+
+  for (int i { -1 }; i < 2; ++i) {
+    for (int j { -1 }; j < 2; ++j) {
+      if ((i == 0 && j == 0) || (i != 0 && j != 0)) continue;
+      auto new_x { x + i };
+      auto new_y { y + j };
+      if (new_x < max_index && new_x > -1 && new_y < max_index && new_y > -1 && !visited[new_x][new_y]) {
+        if (!map[new_x][new_y]) {
+          cout << '[' << x << ':' << y << ']' << endl;
+          traverse(map, visited, new_x, new_y);
+        }
+      }
+    }
+  }
+}
+
+int main() {
+  size_t N { };
+
+  cin >> N;
+
+  Map map { };
+  map.reserve(N);
+
+  for (int i { }; i < N; ++i) {
+    string buf { };
+    buf.reserve(N);
+    cin >> buf;
+    vector<bool> line;
+    line.reserve(N);
+
+    for_each(
+      buf.cbegin(),
+      buf.cend(),
+      [&line](auto item) { line.push_back(item == '.'? false: true); });
+
+    map.push_back(line);
+  }
+
+  print(map);
+
+  size_t walls { };
+
+  Map visited { };
+  visited.reserve(N);
+
+  for (int i { }; i < N; ++i) {
+    auto line = vector<bool>(N, false);
+    visited.push_back(line);
+  }
+
+  traverse(map, visited, 0, 0);
+  print(visited);
+
+  cout << walls * 3;
+
+  return EXIT_SUCCESS;
+}
